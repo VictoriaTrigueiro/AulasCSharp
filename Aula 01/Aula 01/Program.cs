dc6c2b9354595c4de54fcdda448d61bcfd9a24d9
VictoriaTrigueiro <victoria.tbarros@senacsp.edu.br>
2025-02-25 20:33:41 -0300

    Inclusão de Informações.

---
+++ Aula 01/Aula 01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+Console.WriteLine("Hello, World!\n");
+Console.Write("Victoria");
+Console.Write("Trigueiro ");
+
+string nomeCompleto = "Victoria Trigueiro";
+string primeiroNome = "Victoria";
+string ultimoNome = "Trigueiro";
+string nomeMeio = "de Barros";
+Console.WriteLine("Meu nome é " + primeiroNome + nomeMeio + ultimoNome);
+int idade = 20;
+Console.WriteLine($"Meu nome é   {primeiroNome}  {nomeMeio} {ultimoNome} e minha idade é {idade}");
+Console.WriteLine("Escreva o seu Nome  ");
+string nome = Console.ReadLine();
+Console.WriteLine($"Ola {nome}, Boa noite !");
